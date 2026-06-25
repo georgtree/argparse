5b096a3b77a80ea8575f3cb6ce8eae14d667f1fe
Георгий Яшин <georgtree@gmail.com>
2026-06-25 12:46:05 +0100

    documentation migration to ruff!

--- docs/doc_gen.tcl
+++ docs/doc_gen.tcl
@@ -1,16 +1,49 @@
-package require tmdoc
+package require ruff
 package require fileutil
 
-set currentDir [file normalize [file dirname [info script]]]
+set docDir [file dirname [file normalize [info script]]]
+set sourceDir [file join $docDir ..]
+source [file join $docDir argparse.ruff]
 
-::tmdoc::tmdoc [file join $currentDir README.tmd] [file join $currentDir .. README.md]
+set packageVersion [package versions argparse]
+puts $packageVersion
+set title "argparse package"
 
-proc processMd {fileContents} {
-    return [string map {tclcode tcl tclout text} $fileContents]
+set commonSphinx [list -title $title -sortnamespaces false -pagesplit namespace -recurse false\
+                    -includesource false -pagesplit none -autopunctuate true -compact false -includeprivate false\
+                    -product argparse -diagrammer "ditaa --border-width 1" -version $packageVersion\
+                    -copyright "George Yashin" {*}$::argv]
+set commonNroff [list -title $title -sortnamespaces false -pagesplit namespace -recurse false\
+                         -pagesplit none -autopunctuate true -compact false -includeprivate false\
+                         -product argparse -diagrammer "ditaa --border-width 1" -version $packageVersion\
+                         -copyright "George Yashin" {*}$::argv]
+
+set namespaces [list ::argparse]
+
+ruff::document $namespaces -format sphinx -outfile argparse.rst -outdir [file join $docDir sphinx]\
+        {*}$commonSphinx
+ruff::document $namespaces -format nroff -outdir $docDir -outfile argparse.n {*}$commonNroff
+
+::fileutil::appendToFile [file join $docDir sphinx conf.py] {html_theme = "classic"
+extensions = [
+    "sphinx.ext.githubpages",
+]
+from pygments.lexers.tcl import TclLexer
+from pygments.token import Operator
+
+class MyTclLexer(TclLexer):
+    def get_tokens_unprocessed(self, text):
+        for i, t, v in super().get_tokens_unprocessed(text):
+            if v == "=":
+                yield i, Operator, v   # or Name.Builtin
+            else:
+                yield i, t, v
+
+def setup(app): 
+    from sphinx.highlighting import lexers
+    lexers["tcl"] = MyTclLexer()
 }
-fileutil::updateInPlace [file join $currentDir .. README.md] processMd
 
-# nroff generating
-set file [file join $currentDir .. README.md]
-exec pandoc -s -t man [file join $currentDir .. README.md] -o [file join $currentDir argparse.n]
+catch {exec sphinx-build -b html [file join $docDir sphinx] [file join $docDir]} errorStr
+puts $errorStr
 
